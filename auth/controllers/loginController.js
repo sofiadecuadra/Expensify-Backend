b7c501275c2b7df815bd3382fab0ce4d59862f5f
Noelia Bentancor <noeliabentancor1@gmail.com>
2022-09-05 13:07:27 -0300

    adds login controller and auth route

--- auth/controllers/loginController.js
+++ auth/controllers/loginController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const { User } = require("../models/user");
+const { User } = require("../../models/UserSQL");
 
 class loginController {
     static async logIn(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const users = require("./users");
 const expenses = require("./expenses");
-
+const login = require("./auth");
 const routes = Router({ mergeParams: true });
 
 routes.use("/users", users);
 routes.use("/expenses", expenses);
+routes.user("/logIn", login);
 
 module.exports = routes;
