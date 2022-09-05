536f390d1565dd19d3d3f8055e5020be4da719fb
Noelia Bentancor <noeliabentancor1@gmail.com>
2022-09-05 13:03:54 -0300

    Adds login controller

---
+++ auth/loginController.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+
+class loginController {
+    static async logIn(req, res, next) {
+        try {
+            let user = await User.findOne({ email: req.body.email });
+
+            const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+            if (!isValidPassword) throw new LoginError();
+
+            const token = user.createToken();
+            res.send({ token: token });
+        } catch (err) {
+            next(err);
+        }
+    }
+}
+
+module.exports = loginController;
