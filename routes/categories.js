ce29725c55002ba1682937a2f83872eba8e284c2
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-09-08 20:59:08 -0300

    Adds get expenses by period

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,14 @@
-const Router = require('express');
+const Router = require("express");
 const router = Router({ mergeParams: true });
-const categoryController = require('../controllers/categoryController');
-const authMiddleware = require('../middleware/auth');
-const Roles = require('../library/roles');
+const categoryController = require("../controllers/categoryController");
+const authMiddleware = require("../middleware/auth");
+const Roles = require("../library/roles");
 
-router.post('/', authMiddleware([Roles.Administrator]), categoryController.createCategory);
-router.delete('/:categoryId', authMiddleware([Roles.Administrator]), categoryController.deleteCategory);
-router.put('/:categoryId', authMiddleware([Roles.Administrator]), categoryController.updateCategory);
-router.get('/', authMiddleware([Roles.Member, Roles.Administrator]), categoryController.getCategories);
-router.get('/expenses', authMiddleware([Roles.Administrator]), categoryController.getCategoriesWithMoreExpenses);
+router.post("/", authMiddleware([Roles.Administrator]), categoryController.createCategory);
+router.delete("/:categoryId", authMiddleware([Roles.Administrator]), categoryController.deleteCategory);
+router.put("/:categoryId", authMiddleware([Roles.Administrator]), categoryController.updateCategory);
+router.get("/", authMiddleware([Roles.Member, Roles.Administrator]), categoryController.getCategories);
+router.get("/expenses", authMiddleware([Roles.Administrator]), categoryController.getCategoriesWithMoreExpenses);
+router.get("/expenses/period", authMiddleware([Roles.Administrator]), categoryController.getCategoriesExpensesByPeriod);
 
 module.exports = router;
