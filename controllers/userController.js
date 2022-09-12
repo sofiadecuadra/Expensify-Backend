f557804cb37b213f0a1be0e2ce121ff3fcff4265
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-09-11 21:03:46 -0300

    Creates transaction to rollback when user validation fails

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,6 @@
 const UserSQL = require("../models/userSQL");
 const FamilyController = require("./familyController");
 const bcrypt = require("bcrypt");
-const UserDTO = require("../DTO/userDTO");
 const createKey = require("../library/jwtSupplier");
 const ValidationError = require('../errors/ValidationError');
 const DuplicateError = require('../errors/DuplicateUserError');
@@ -11,10 +10,15 @@ class UserController {
     static async createNewUser(req, res, next) {
         try {
             const { name, email, role, familyName, password } = req.body;
-            const family = await FamilyController.createNewFamily(familyName);
-            const salt = await bcrypt.genSalt(10);
-            const encriptedPassword = await bcrypt.hash(password.toString(), salt);
-            const user = await UserSQL.instance.create({ name, email, role, familyId: family.dataValues.id, password: encriptedPassword });
+
+            const user = await UserSQL.connection.transaction(async (t) => {
+                const family = await FamilyController.createNewFamily(familyName, { transaction: t });
+                const salt = await bcrypt.genSalt(10);
+                const encriptedPassword = await bcrypt.hash(password.toString(), salt);
+                const user = await UserSQL.instance.create({ name, email, role, familyId: family.dataValues.id, password: encriptedPassword }, { transaction: t });
+                return user;
+            });
+
             const data = { userId: user.id, role: user.role, email: user.email, familyId: user.familyId };
             const token = await createKey(data);
             res.send({ token: token });
