baa4669db913fbd3c3796c7a4ae4365d5d6eb50d
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-09-15 20:04:41 -0300

    Adds family fields missing validators

--- controllers/familyController.js
+++ controllers/familyController.js
@@ -1,11 +1,10 @@
-const FamilySql = require('../models/familySQL');
-const createKey = require('../library/jwtSupplier');
-const DuplicateError = require('../errors/DuplicateFamilyError');
+const FamilySql = require("../models/familySQL");
+const createKey = require("../library/jwtSupplier");
+const DuplicateError = require("../errors/DuplicateFamilyError");
 const sequelize = require("sequelize");
 const { WordValidator } = require("../utilities/inputValidators");
 
 class FamilyController {
-
     static async createNewFamily(name, transaction) {
         try {
             const apiKey = await FamilyController.createApiKey(name);
@@ -13,7 +12,7 @@ class FamilyController {
             const family = await FamilySql.instance.create({ name, apiKey }, transaction);
             return family;
         } catch (err) {
-            if (err instanceof sequelize.UniqueConstraintError) throw (new DuplicateError(name))
+            if (err instanceof sequelize.UniqueConstraintError) throw new DuplicateError(name);
             throw err;
         }
     }
@@ -21,10 +20,12 @@ class FamilyController {
     static async updateApiKey(req, res, next) {
         try {
             const { familyId } = req.params;
+            NumberValidator.validate(familyId, "family id", 1000000000);
+
             const family = await FamilySql.instance.findOne({ where: { id: familyId } });
             const apiKey = await FamilyController.createApiKey(family.name);
             await FamilySql.instance.update({ apiKey }, { where: { id: familyId } });
-            res.status(200).json({ message: 'Family API KEY updated successfully' });
+            res.status(200).json({ message: "Family API KEY updated successfully" });
         } catch (err) {
             console.log(err.message);
             next(err);
@@ -36,9 +37,7 @@ class FamilyController {
         const data = { createdAt: todayDate, name: familyName };
         const apiKey = await createKey({ data });
         return apiKey;
-
     }
-
 }
 
 module.exports = FamilyController;
