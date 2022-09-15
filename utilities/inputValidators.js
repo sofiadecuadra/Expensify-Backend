a36dedaca9e02222c5d9d1b9e2651f2841742e36
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-09-15 17:40:13 -0300

    Corrects password validator to work with 'root' password

--- utilities/inputValidators.js
+++ utilities/inputValidators.js
@@ -34,9 +34,9 @@ class EmailValidator {
 
 class PasswordValidator {
     static validate = (input) => {
-        const regex = new RegExp(`^[^ ]{10,64}$`);
+        const regex = new RegExp(`^[^ ]{4,64}$`);
         if (!regex.test(input))
-            throw new InputValidationError(`Please enter a password between 10 and 64 characters (letters, numbers or symbols)!`);
+            throw new InputValidationError(`Please enter a password between 4 and 64 characters (letters, numbers or symbols)!`);
     }
 }
 module.exports = WordValidator, ParagraphValidator, NumberValidator, EmailValidator, PasswordValidator;
