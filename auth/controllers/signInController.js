622d88c4798ed5badff8ea0cd3bfe1c0ac1685ce
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-09-17 01:04:31 -0300

    Fixes signin

--- auth/controllers/signInController.js
+++ auth/controllers/signInController.js
@@ -1,18 +1,18 @@
 const bcrypt = require("bcrypt");
 const UserSQL = require("../../models/userSQL");
-const { createToken } = require("../../library/jwtSupplier");
+const { createKey } = require("../../library/jwtSupplier");
 
 class SignInController {
 
     static async logIn(req, res, next) {
         try {
-            console.log(UserSQL);
             let user = await UserSQL.instance.findOne({ email: req.body.email });
 
             const isValidPassword = await bcrypt.compare(req.body.password, user.password);
             //TO DO: handle errors
             const data = { userId: user.id, role: user.role, email: user.email, familyId: user.familyId };
-            const token = await createToken(data);
+            const token = await createKey(data);
+            console.log(data)
             res.send({ token: token });
         } catch (err) {
             next(err);
