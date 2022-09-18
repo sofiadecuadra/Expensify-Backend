bb3a87d5adac031abdac9ff6ae7fa3e527ccff86
NoeliaBentancor <noeliabentancor1@gmail.com>
2022-09-18 13:30:05 -0300

    Adds current url for invitations

--- library/emailSender.js
+++ library/emailSender.js
@@ -2,7 +2,7 @@ const sgMail = require('@sendgrid/mail');
 const config = require('config');
 const emailApiKey = config.get("EMAIL_SERVICE.api_key");
 const emailSender = config.get("EMAIL_SERVICE.sender");
-const url=config.get()
+const url = config.get("URL_INVITATION")
 sgMail.setApiKey(emailApiKey);
 
 const getMessage = async (emails, inviteToken) => {
@@ -11,7 +11,7 @@ const getMessage = async (emails, inviteToken) => {
         from: emailSender,
         subject: 'Invitation to join the family',
         text: 'You have been invited to join the family',
-        html: `<strong>Click on the link to join the family: http://localhost:3000/families/${inviteToken}</strong>`,
+        html: `<strong>Click on the link to join the family: ${url}${inviteToken}</strong>`,
     };
     return message;
 };
@@ -22,7 +22,7 @@ const sendEmail = async (emails, inviteToken) => {
     sgMail
         .send(message)
         .then((response) => {
-           
+
         })
         .catch((error) => {
             console.error(error)
