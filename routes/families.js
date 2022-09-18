8b784b4ddb485b69c84845b8d387308de81d099d
sofiadecuadra <sofidecuadra@gmail.com>
2022-09-18 12:41:20 -0300

    Adds endpoint to get api key

--- routes/families.js
+++ routes/families.js
@@ -4,7 +4,8 @@ const authMiddleware = require('../middleware/auth');
 const FamilyController = require('../controllers/familyController');
 const Roles = require('../library/roles');
 
-router.patch('/:familyId/apiKey/', FamilyController.updateApiKey);
+router.get('/apiKey/', authMiddleware([Roles.Administrator]), FamilyController.getApiKey);
+router.patch('/apiKey/', authMiddleware([Roles.Administrator]), FamilyController.updateApiKey);
 router.post('/:familyId/invite/:userType/', authMiddleware([Roles.Administrator]), FamilyController.createInvite);
 router.get('/:inviteToken/', FamilyController.validateInviteToken);
 
