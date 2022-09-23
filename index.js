1750086ead362319fcbb9eb4151adb375d3beadf
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-09-22 21:31:00 -0300

    Adds health check

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { FamilySQL, CategorySQL, ExpenseSQL, UserSQL } = require("./models");
+const HealthCheckController = require("./controllers/healthCheckController");
 const config = require("config");
 const dbPort = config.get("MYSQL_DB.port");
 
-(async() => {
+(async () => {
     const SequelizeContext = require("./startup/SequelizeContext");
     const sequelizeContext = new SequelizeContext(dbPort);
     await FamilySQL.createInstance(sequelizeContext);
@@ -14,9 +15,10 @@ const dbPort = config.get("MYSQL_DB.port");
         .sync()
         .then(() => console.log("Database is connected!"))
         .catch((err) => console.error(err, "Something went wrong, database is not connected!"));
+    HealthCheckController.connection = sequelizeContext.connection;
 })();
 
 const Server = require("./server");
 const server = new Server();
 server.start();
-server.config(); // <-- this is the line that is causing the error
+server.config();
