62db2903f28c201f288b15b2413abf257d84bcd3
Noelia Bentancor <noeliabentancor1@gmail.com>
2022-09-27 11:51:49 -0300

    Adds sample test

--- performanceTests/performanceTest.java
+++ performanceTests/performanceTest.java
@@ -7,18 +7,35 @@ import java.time.Instant;
 import org.junit.jupiter.api.Test;
 import us.abstracta.jmeter.javadsl.core.TestPlanStats;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+import static us.abstracta.jmeter.javadsl.dashboard.DashboardVisualizer.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
 public class PerformanceTest {
 
   @Test
-  public void testPerformance() throws IOException {
-    TestPlanStats stats = testPlan(
-        threadGroup(2, 10,
-            httpSampler("http://my.service")
-        ),
-        //this is just to log details of each request stats
-        jtlWriter("target/jtls")
-    ).run();
-    assertThat(stats.overall().sampleTimePercentile99()).isLessThan(Duration.ofSeconds(5));
-  }
+  public void testSimple() throws IOException {
+    String reportDir = "./report/testSimple";
 
+    TestPlanStats stats = testPlan(
+        threadGroup(
+            10,
+            10,
+            httpSampler("https://google.com/"),
+            uniformRandomTimer(500, 2000)),
+        dashboardVisualizer(),
+        htmlReporter(reportDir)).run();
+
+    assertThat(stats.overall().sampleTimePercentile99()).isLessThan(Duration.ofSeconds(2));
+  }
 }
