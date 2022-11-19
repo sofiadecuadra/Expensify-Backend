bf51477179ed65c92ffb8156d6954d524f0b32fa
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-11-19 17:21:40 -0300

    Finishes mongo delete

--- businessLogic/healthCheckLogic.js
+++ businessLogic/healthCheckLogic.js
@@ -2,20 +2,13 @@ const HealthCheckError = require("../errors/HealthCheckError");
 
 class HealthCheckLogic {
     sqlConnection;
-    mongoDb;
 
-    constructor(sqlConnection, mongoDb) {
+    constructor(sqlConnection) {
         this.sqlConnection = sqlConnection;
-        this.mongoDb = mongoDb;
     }
 
     async healthCheck() {
         try {
-            await this.mongoDb.db("admin").command({ ping: 1 });
-            const isConnected = !!this.mongoDb && !!this.mongoDb.topology && this.mongoDb.topology.isConnected();
-            if (!isConnected) {
-                throw new HealthCheckError("MongoDB Database is not connected");
-            }
             await this.sqlConnection.authenticate();
             const result = {
                 uptime: process.uptime(),
